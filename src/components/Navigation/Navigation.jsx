fc2e2601e72b6510d702ee2f6928a5773213e02f
Jacek Pasierb <jack_byk@o2.pl>
2023-07-15 14:03:32 +0200

    add icon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../Hooks/useAuth";
 import css from "./Navigation.module.css";
-
+import HomeIcon from "@mui/icons-material/Home";
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
   return (
     <nav>
-      <NavLink  className={css.link} to="/">
+      <NavLink className={css.link} to="/" style={{ display:"flex", alignContent: "center" }}>
+        <HomeIcon sx={{ fontSize: 40, marginRight: 2 }} />
         Home
       </NavLink>
       {isLoggedIn && (
